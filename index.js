fd594f643305cac3f1a1b37658b837bbd8f8af4b
Hexel Colorado <=>
2025-03-17 14:40:01 -0500

    Add temporary test to see if env variables are being read on production

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -9,6 +10,21 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   res.json({ message: "Hello, world! Your API is running." });
+});
+
+app.get("/test", (req, res) => {
+  const teamupMainCalendarId = process.env.TEAMUP_DALLAS_URBANISTS_COMMUNITY_CALENDAR_MODIFIABLE_ID;
+  const teamupSubcalendarId = '14173954';
+  const minWaitSeconds = process.env.MIN_WAIT_SECONDS ?? 2;
+  const maxWaitSeconds = process.env.MAX_WAIT_SECONDS ?? 5;
+  const teamupApiKey = process.env.TEAMUP_APIKEY;
+  res.json({
+    teamupMainCalendarId,
+    teamupSubcalendarId,
+    minWaitSeconds,
+    maxWaitSeconds,
+    teamupApiKey
+  })  
 });
 
 app.get("/api/dn4h/sync", (req, res) => {
