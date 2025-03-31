f5c60bf9b42ef56111f47ddd79c8e51803e3cfa9
Hexel Colorado <=>
2025-03-30 19:54:03 -0500

    Set environment for Github Actions workflows

--- operations/scrape-icalendar-sources.ts
+++ operations/scrape-icalendar-sources.ts
@@ -2,11 +2,14 @@ import { ICalendar } from "../scrapers/ICalendar";
 import { Sources } from "../models/Sources";
 import { initEventModel } from "../models/Event";
 import { initSavedScrap } from "../models/Scrap";
+import { sequelize } from "../database/sequelize";
 
 initEventModel();
 initSavedScrap();
 
 async function main() {
+  await sequelize.authenticate();
+
   for (let source of Sources.byPlatform('icalendar')) {
     const scraper = new ICalendar(source.url, source.org);
     await scraper.updateDatabase();
