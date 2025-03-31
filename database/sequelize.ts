9ede87d1d0aecbe11446471449552c3c5e144181
Hexel Colorado <=>
2025-03-30 20:00:36 -0500

    Attempt fix SSL/TLS requirement issue

--- database/sequelize.ts
+++ database/sequelize.ts
@@ -3,5 +3,11 @@ import { Sequelize } from 'sequelize';
 
 export const sequelize = new Sequelize(process.env.DATABASE_URL!, {
   dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true, // Enforce SSL
+      rejectUnauthorized: false, // Allow self-signed certificates (optional, depending on your database setup)
+    },
+  },
   logging: false, // Disable logging for cleaner output
 });
